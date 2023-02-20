3f9ae42a2942f2d10d02947f1ae2cb951ab76dfa
Lugas L H <lugas.luqman@gmail.com>
2023-02-20 10:26:33 +0700

    finishing client class

--- src/Client.php
+++ src/Client.php
@@ -1,6 +1,7 @@
 <?php
 namespace Lugasdev\Mynextcloud;
 
+use Lugasdev\Mynextcloud\Exception\ClientException;
 use Sabre\DAV\Client as DAVClient;
 
 class Client {
@@ -20,6 +21,14 @@ class Client {
         $this->client = new DAVClient($this->setting);
     }
 
+    /**
+     * get directory
+     *
+     * @param string $directoryPath
+     * @return array
+     *
+     * @author Lugas Luqman Hakim <lugas.luqman@gmail.com>
+     */
     public function getDirectory($directoryPath)
     {
         try {
@@ -42,10 +51,103 @@ class Client {
             }
 
         } catch (\Exception $e) {
-            return false;
+            throw new ClientException($e->getMessage());
         }
 
         return $files;
     }
 
+    /**
+     * upload file to directory
+     *
+     * @param string $dirPathTarget target directory
+     * @param string $filePath local file path
+     * @param string $fileName file name
+     * @return array
+     * @author Lugas Luqman Hakim <lugas.luqman@gmail.com>
+     */
+    public function upload($dirPathTarget, $filePath, $fileName = '')
+    {
+        if (!is_file($filePath)) {
+            throw new ClientException('File Path is not valid');
+        }
+        $file = file_get_contents($filePath);
+
+        if (empty($fileName)) {
+            $fileName_ = explode('/', $filePath);
+            $fileName = end($fileName_);
+        }
+
+        $filePath = trim($dirPathTarget, '/') . "/" . $fileName;
+        $urlPath  = $this->setting['baseUri'] . "files/" . $this->setting['userName'] . '/' . $filePath;
+
+        $response = $this->client->request('PUT', $urlPath , $file);
+
+        return array_merge($response, ["filePath" => $filePath, "urlPath" => $urlPath, 'fileName' => $fileName]);
+    }
+
+    /**
+     * delete file
+     *
+     * @param string $filePath nextcloud file path
+     * @return string
+     * @author Lugas Luqman Hakim <lugas.luqman@gmail.com>
+     */
+    public function delete($filePath)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL            => $this->setting['baseUri'] . "/dav/files/". $this->setting['userName'] ."/" . $filePath,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => "",
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => "DELETE",
+            CURLOPT_HTTPHEADER     => array(
+                "Authorization: Basic " . base64_encode( $this->setting['userName'] . ":" . $this->setting['password']),
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $response;
+    }
+
+    /**
+     * create directory
+     *
+     * @param string $dirPath directory to create
+     * @return boolean
+     * @author Lugas Luqman Hakim <lugas.luqman@gmail.com>
+     */
+    public function createDir($dirPath)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL            => $this->setting['baseUri'] . "/files/" . $this->setting['userName'] . "/" . $dirPath,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => "",
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => "MKCOL",
+            CURLOPT_HTTPHEADER     => array(
+                "Authorization: Basic " . base64_encode( $this->setting['userName'] . ":" . $this->setting['password']),
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        // echo $response;
+
+        return true;
+    }
 }
---
+++ src/Exception/ClientException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Lugasdev\Mynextcloud\Exception;
+
+/**
+ * Exception when a client error is encountered (4xx codes)
+ */
+class ClientException extends \Exception
+{
+}
