2162c59fa86ce851f1b186e71ba2b0e0e77049d7
Lugas L H <lugas.luqman@gmail.com>
2023-05-15 15:59:27 +0700

    create share function

---
+++ src/ShareType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Lugasdev\Mynextcloud;
+
+class ShareType {
+
+    /**
+     * 0 = user
+     */
+    const USER = 0;
+
+    /**
+     * 1 = group
+     */
+    const GROUP = 2;
+
+    /**
+     * 3 = public link
+     */
+    const PUBLIC_LINK = 3;
+
+    /**
+     * 4 = email
+     */
+    const EMAIL = 4;
+
+    /**
+     * 6 = federated cloud share
+     */
+    const FEDERATED = 6;
+
+    /**
+     * 7 = circle
+     */
+    const CIRCLE = 7;
+
+    /**
+     * 10 = Talk conversation
+     */
+    const TALK = 7;
+}
